8ebdd1848d42d80479f93ac20fc50806295344ea
CodingKittens247 <afonso.borlido@proton.me>
2025-08-05 13:35:33 +0100

    changed the main method to incorporate an external crate, with logic, changed the function countin to use a vector, for the future to use slices better

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 //use std::collections::HashMap;
 use countingapp::countint::countint;
 fn main() {
-    countint();
+    let nums: Vec<i32> = vec![1,2,3,4,5,6,1,2,3,1,2,1,2,1,7,9,6,6,6,6,102,100,9001];
+    countint(nums);
 }
