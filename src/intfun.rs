bcbd7010d590c24dd05d5a725eededa0c30d77ba
CodingKittens247 <afonso.borlido@proton.me>
2025-08-06 15:33:58 +0100

    push update ntfun refactor

--- src/intfun.rs
+++ src/intfun.rs
@@ -1,16 +1,16 @@
 
-    pub fn medianint(nums: &[i32]) -> i32 {
+    pub fn medianint(nums: &[i32]) -> f64 {
     //my modus operandi here is gonna be, take the vectored list, parse it (order it) and met len it, and return the middle one if its a prime
     //that was my first approach, but i refactored it into taking a slice of the entire vector, so i could easierly modify it without much hassle.
                //let count: usize = nums.len(); // we need to know what the size of the vector is, so we can determine its length and if its div by 2
      let sorted: Vec<i32> = nums.to_vec(); // we make a new vec from the slice, so we dont take ownership and modify the og input
      let len = sorted.len();
-     let end: i32 = if len % 2 == 0 
+     let end: f64 = if len % 2 == 1 
      {  
-      sorted[len / 2]
+      sorted[len / 2] as f64 //we grab the copied vec, and index half the len, t find half if it is divissible by 2
         
      } else {
-        (sorted[len / 2] + sorted[(len / 2) - 1]) / 2 //in this case, if its not even, we will just grab the 2 middle elements of the vector, and divide divide their value, that way we can kinda get the median lol 
+        (sorted[len / 2] + sorted[(len / 2) - 1]) as f64 / 2.0 //in this case, if its not even, we will just grab the 2 middle elements of the vector, and divide divide their value, that way we can kinda get the median lol 
      };
-     return end
+     return end as f64
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //use std::collections::HashMap;
 use chall83::intfun::medianint;
 fn main() {
-    let nums: Vec<i32> = vec![1,2,3];
+    let nums: Vec<i32> = vec![1,2,3,4];
     println!("{}",medianint(&nums));
 }
